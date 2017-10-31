c1fbc6f732ca861b314966dacc46692d9249752c
Saurabh mendhe <Saurabh.mendhe@gmail.com>
2017-11-01 01:00:00 +0530

    Initial Commit

---
+++ MVImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVImage
+//
+//  Created by Saurabh Mendhe on 01/11/17.
+//  Copyright © 2017 Saurabh Mendhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVImage
+//
+//  Created by Saurabh Mendhe on 01/11/17.
+//  Copyright © 2017 Saurabh Mendhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
